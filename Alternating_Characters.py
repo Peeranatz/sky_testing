2ccc2b83104f1b0b1b29ec0c148b5660027ba9c3
Peeranat Chunhok <Peeranat280648@gmail.com>
2025-03-02 19:10:07 +0700

    Add testing case Alternating_Characters.py

--- Alternating_Characters.py
+++ Alternating_Characters.py
@@ -8,3 +8,20 @@ def alternating_characters(s):
         if s[i] == s[i - 1]:
             deletions += 1
     return deletions
+
+
+class TestAlternatingCharacters(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(alternating_characters("AABAAB"), 2)
+
+    def test_no_deletions(self):
+        self.assertEqual(alternating_characters("ABABABAB"), 0)
+
+    def test_all_same(self):
+        self.assertEqual(alternating_characters("AAAA"), 3)
+
+    def test_empty_string(self):
+        self.assertEqual(alternating_characters(""), 0)  # กรณีสตริงว่าง
+
+    def test_single_character(self):
+        self.assertEqual(alternating_characters("A"), 0)  # กรณีมีตัวอักษรเดียว
