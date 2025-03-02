0192e479f1fba18e706edaa5020d79c62515da6f
Peeranat Chunhok <Peeranat280648@gmail.com>
2025-03-02 19:42:23 +0700

    Add testing case  cat_and_mouse.py

--- cat_and_mouse.py
+++ cat_and_mouse.py
@@ -10,3 +10,20 @@ def cat_and_mouse(x, y, z):
         return "Cat B"
     else:
         return "Mouse C"
+
+
+class TestCatAndMouse(unittest.TestCase):
+    def test_cat_a_wins(self):
+        self.assertEqual(cat_and_mouse(1, 5, 2), "Cat A")
+
+    def test_cat_b_wins(self):
+        self.assertEqual(cat_and_mouse(2, 5, 4), "Cat B")
+
+    def test_mouse_escapes(self):
+        self.assertEqual(cat_and_mouse(1, 3, 2), "Mouse C")
+
+    def test_equal_distance(self):
+        self.assertEqual(cat_and_mouse(4, 6, 5), "Mouse C")
+
+    def test_large_values(self):
+        self.assertEqual(cat_and_mouse(10, 100, 50), "Cat A")
