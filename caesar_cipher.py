98d1bcccb49fd4e1465f405a29f6430491131c1f
Peeranat Chunhok <Peeranat280648@gmail.com>
2025-03-02 19:17:30 +0700

    Add testing caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -11,3 +11,23 @@ def caesar_cipher(s, k):
         else:
             result += char
     return result
+
+
+class TestCaesarCipher(unittest.TestCase):
+    def test_shift_1(self):
+        self.assertEqual(caesar_cipher("abc", 1), "bcd")
+
+    def test_shift_26(self):
+        self.assertEqual(caesar_cipher("abc", 26), "abc")
+
+    def test_with_special_characters(self):
+        # ทดสอบกรณีมีสัญลักษณ์พิเศษ
+        self.assertEqual(caesar_cipher("a!b@c", 1), "b!c@d")
+
+    def test_uppercase_letters(self):
+        # ทดสอบกรณีตัวอักษรใหญ่
+        self.assertEqual(caesar_cipher("ABC", 1), "BCD")
+
+    def test_mixed_case_and_symbols(self):
+        # ทดสอบกรณีมีทั้งตัวใหญ่ ตัวเล็ก และสัญลักษณ์
+        self.assertEqual(caesar_cipher("AbC!xYz", 2), "CdE!zAb")
