fa0bfbb34ce350c079df63245d7d46830396c6d4
Peeranat Chunhok <Peeranat280648@gmail.com>
2025-03-02 18:54:05 +0700

    Add testing

--- funnyString.py
+++ funnyString.py
@@ -8,3 +8,20 @@ def funny_string(s):
     reversed_diff = original_diff[::-1]
     # Check if they are equal
     return "Funny" if original_diff == reversed_diff else "Not Funny"
+
+
+class TestFunnyString(unittest.TestCase):
+    def test_funny(self):
+        self.assertEqual(funny_string("acxz"), "Funny")
+
+    def test_not_funny(self):
+        self.assertEqual(funny_string("bcxz"), "Not Funny")
+
+    def test_single_char(self):
+        self.assertEqual(funny_string("a"), "Funny")  # Edge case with one character
+
+    def test_palindrome(self):
+        self.assertEqual(funny_string("abccba"), "Funny")  # Palindrome case
+
+    def test_all_same(self):
+        self.assertEqual(funny_string("aaaa"), "Funny")  # All characters same
